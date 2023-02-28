074a4279cd62c65d25214853d3021d0aa43a0a29
Veneta <venkireva80@gmail.com>
2023-02-28 12:14:34 +0200

    Combine height and width

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from matplotlib import pyplot as plt, cm
+from matplotlib import pyplot as plt
 
 from barnsley_fern import barnsley_fern
 
@@ -12,8 +12,7 @@ st.title("Barnsley Fern Generator 🌿")
 
 num_points = st.select_slider('Number of points', value=10_000, options=[10 ** x for x in [*range(1, 7)]])
 
-width = st.slider('Image width (px)', value=500, min_value=200, max_value=2000, step=10)
-height = st.slider('Image height (px)', value=500, min_value=200, max_value=2000, step=10)
+size_px = st.slider('Image size (px)', value=500, min_value=200, max_value=2000, step=10)
 
 color_map = st.selectbox(
     'Select color scheme',
@@ -32,7 +31,7 @@ ax.set_yticks([], [])
 plt.figure(dpi=300)
 
 try:
-    img = ax.imshow(barnsley_fern(num_points, width, height)[::-1, :], interpolation="bicubic", cmap=color_map)
+    img = ax.imshow(barnsley_fern(num_points, size_px, size_px)[::-1, :], interpolation="bicubic", cmap=color_map)
     st.pyplot(fig)
 except IndexError:
     st.error('Image size is invalid, please enter another one.', icon='⚠️')
